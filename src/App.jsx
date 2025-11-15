d99ae00ee27edf8e49e7925fe23e42de4e8975a5
raymondddenny <denny.raymond@sprout.co.id>
2025-11-15 19:13:50 +0700

    feat: finish art section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import gsap from 'gsap';
-import { ScrollTrigger,SplitText } from 'gsap/all';
-import Hero from './components/Hero';
-import Cocktails from './components/Cocktails';
-import Navbar from './components/Navigation';
-import About from './components/About';
-
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
+import Navbar from "./components/Navigation";
+import About from "./components/About";
+import Art from "./components/Art";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function App() {
-  return (
-  <main>
-    <Navbar />
-    <Hero />
-    <Cocktails />
-    <About/>
-  </main>
-  );
+	return (
+		<main>
+			<Navbar />
+			<Hero />
+			<Cocktails />
+			<About />
+			<Art />
+		</main>
+	);
 }
