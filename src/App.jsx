7edb3c6e3a08332639894e9cb6336ead70cc6994
raymondddenny <denny.raymond@sprout.co.id>
2025-11-15 18:45:03 +0700

    feat: finish about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger,SplitText } from 'gsap/all';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
 import Navbar from './components/Navigation';
+import About from './components/About';
 
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -13,6 +14,7 @@ export default function App() {
     <Navbar />
     <Hero />
     <Cocktails />
+    <About/>
   </main>
   );
 }
