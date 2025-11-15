77ea8ecb40e962aa3c3ca364d38a7c58db05452f
raymondddenny <denny.raymond@sprout.co.id>
2025-11-15 14:10:24 +0700

    fix: build

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import gsap from 'gsap';
 import { ScrollTrigger,SplitText } from 'gsap/all';
-import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
+import Navbar from './components/navbar';
 
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
