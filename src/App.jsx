4549b52da4aefc5dd5b43bac10610cf95cf6d073
raymondddenny <denny.raymond@sprout.co.id>
2025-11-15 14:11:29 +0700

    Fix navbar import path casing

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import gsap from 'gsap';
 import { ScrollTrigger,SplitText } from 'gsap/all';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
